0c333b1519e9d1e127088c382fb3eab2fee1098a
Martin Hernandez <hernandez1399@gmail.com>
2021-10-05 04:04:07 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let profile = {
+  profilePic: "Martin",
+  fullName: "Martin Hernandez",
+  age: 30,
+  location: "Los Angeles",
+  contact: {
+    instagram: "https://www.instagram.com/the_akward_programmer/",
+    email: "mailto:Hernandezwork1399@gmail.com",
+    github: "https://github.com/Justmartin626",
+  },
+};
+
+document.getElementById("fullname").innerText = profile.fullName;
+document.getElementById("age").innerText = profile.age;
+document.getElementById("instagram").href = profile.contact.instagram;
+document.getElementById("email").href = profile.contact.email;
+document.getElementById("github").href = profile.contact.github;
+console.log(profile.age);
